60e0fdf9c6cea739317d3d9a25af43a6d1f3e9ea
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-14 20:31:25 -0400

    completed test

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,12 +1,12 @@
 class Vehicle:
+    
     def __init__(self, wheel_size, wheel_number):
-        self.wheel_size= wheel_size
-        self.wheen_number = wheel_number
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
 
     def go(self):
         return "vrrrrrrrooom!"
 
     def fill_up_tank(self):
-        return "filling_up!"
+        return "filling up!"
 
-        
